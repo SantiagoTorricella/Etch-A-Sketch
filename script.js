97711414533967eb0352ed26fb515ae9ccf7fb04
Santiago Torricella <torrisantiago@gmail.com>
2022-10-12 10:10:37 -0300

    Grid terminado

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Seteo las variables
 const container = document.getElementById("container");
 
+
+// Crea un grid dinamico tomando los valores de fila y columna
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
+  container.style.setProperty('--grid-rows', rows); // Valores para el grid dinamico
+  container.style.setProperty('--grid-cols', cols); // Valores para el grid dinamico
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     cell.innerText = (c + 1);
@@ -11,3 +14,7 @@ function makeRows(rows, cols) {
 };
 
 makeRows(16, 16);
+
+
+
+
