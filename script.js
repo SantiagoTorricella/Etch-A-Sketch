5aa7d377de687848bb072601d34b054effbd027d
Santiago Torricella <torrisantiago@gmail.com>
2022-10-17 12:28:01 -0300

    Hover terminado

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 // Seteo las variables
 const container = document.getElementById("container");
 
-
 // Crea un grid dinamico tomando los valores de fila y columna
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows); // Valores para el grid dinamico
-  container.style.setProperty('--grid-cols', cols); // Valores para el grid dinamico
-  for (c = 0; c < (rows * cols); c++) {
+  container.style.setProperty("--grid-rows", rows); // Valores para el grid dinamico
+  container.style.setProperty("--grid-cols", cols); // Valores para el grid dinamico
+  for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
-  };
-};
+  }
+  hover();
+}
 
-makeRows(16, 16);
+function randomColor() {
+  let letras = "012345679abcdef";
+  let color = "#";
+  for (i = 0; i < 6; i++) {
+    color += letras[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
+function hover() {
+  let items = document.querySelectorAll(".grid-item");
+  items.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = `${randomColor()}`;
+    });
+  });
+}
 
+// Variables para el grid
 
+let columnas = 16;
+let filas = 16;
 
+makeRows(filas, columnas);
